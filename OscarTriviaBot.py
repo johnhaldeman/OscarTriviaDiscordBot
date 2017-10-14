590f8a5119ddcdcd5b35500efb128f7e1d00a8b6
John Haldeman <john.haldeman@infoinsightsllc.com>
2017-10-13 20:38:01 -0400

    Improve keyword searches - extracting link descriptions of plot/cast sections

--- OscarTriviaBot.py
+++ OscarTriviaBot.py
@@ -4,13 +4,19 @@ import urllib.request
 f = urllib.request.urlopen("https://en.wikipedia.org/wiki/Fantastic_Beasts_and_Where_to_Find_Them_(film)")
 soup = BeautifulSoup(f, "html.parser")
 
+keywords = [];
+
 for header in soup.find_all(['h2']):
     span = header.span
     if span != None and span['id'] in ('Plot', 'Cast'):
-        print(span['id'])
         for elem in header.next_siblings:
             if elem.name and elem.name.startswith('h'):
                 break;
-            if elem.name == 'p':
-                print(elem.get_text())
+            elif elem.name:
+                for link in elem.select('a'):
+                    text = link.get_text();
+                    if not text.startswith('[') and not text == '':
+                        keywords.append(text)
 
+for keyword in keywords:
+    print(keyword)
